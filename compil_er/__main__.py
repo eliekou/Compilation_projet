daeba357a787c6fa51ca28ac3707eb95bd6936f8
eliekou <elie.kouyoumdjian@ensta-bretagne.org>
2023-03-22 16:30:56 +0100

    tryfirstparser and examples

---
+++ compil_er/__main__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+import sys
+from lex_er import Lexer
+#from p4_rser import Parser
+
+if __name__ == "__main__":
+    lexer = Lexer()
+    print(lexer.lex_file(sys.argv[1]))
+    #parser = Parser(lexer.lex_file(sys.argv[1]))
+    #print(parser.parse())
+    # 
